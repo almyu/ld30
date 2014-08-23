1a9b2c0e13db25b0729cd96156b917c7a032f87d
almyu <hire@indeed.li>
2014-08-23 20:01:10 +0700

    Add riding test

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Car : MonoBehaviour {
+
+    public float acceleration = 20.0f;
+
+    private Rigidbody2D cachedBody;
+
+    private void Awake() {
+        cachedBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        var dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        cachedBody.AddForce(dir * Time.deltaTime * acceleration, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Utility/Scripts/RectTransformExt.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public static class RectTransformExt {
+
+    public static void AddChild(this RectTransform xf, RectTransform other) {
+        var initialPosition = other.anchoredPosition;
+        var initialSize = other.sizeDelta;
+
+        other.parent = xf;
+
+        other.anchoredPosition = initialPosition;
+        other.sizeDelta = initialSize;
+    }
+}
