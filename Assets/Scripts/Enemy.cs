b0d9ed51075fb2938464041e2e7ed71869f4071c
Anton Tarasov <uchiro.me@gmail.com>
2014-08-24 02:03:28 +0700

    Add simple AI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,17 +5,28 @@ public class Enemy : MonoBehaviour {
     
     public float lifeDistance = 30.0f;
     
+    private Transform player;
     private Transform cachedTransform;
     private Transform cachedTransformCamera;
+    private Car cachedCar;
     
     private bool isLive = true;
 
     private void Awake() {
         cachedTransform = transform;
         cachedTransformCamera = Camera.main.transform;
+        
+        cachedCar = GetComponent<Car>();
+    }
+    
+    private void Start() {
+        player =  PlayerController.instance.transform;
     }
     
     private void Update() {
+        var angle = Vector3.Angle(cachedCar.inner.up, player.position - cachedTransform.position);
+        var sign = Mathf.Sign(Vector3.Dot(Vector3.back,Vector3.Cross(cachedCar.inner.up, player.position - cachedTransform.position)));
+        cachedCar.control = new Vector2(sign * Mathf.Lerp(0.0f, 1.0f, angle), 1.0f);
         if (Vector3.Distance(cachedTransform.position, cachedTransformCamera.position) >= lifeDistance)
             Remove();
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,17 @@
 [RequireComponent(typeof(Car))]
 public class PlayerController : MonoBehaviour {
 
+    public static PlayerController instance;
+    
     private Car cachedCar;
 
     private void Awake() {
+        instance = this;
+        
         cachedCar = GetComponent<Car>();
     }
 
     private void Update() {
-        cachedCar.control = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+        cachedCar.control = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
 }
