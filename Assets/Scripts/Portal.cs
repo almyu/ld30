d6f15305dc00f22f36a62ad8669945e6cb3bc323
Anton Tarasov <uchiro.me@gmail.com>
2014-08-24 23:16:41 +0700

    Fix collider in player

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -3,17 +3,28 @@ using System.Collections;
 
 public class Portal : MonoBehaviour {
 
-    public int index = 1;
+    public LevelSettings.LevelName course;
+
+    public Transform glow;
+
+    public float speedRotation = 1.0f;
 
     private void Awake() {
     
     }
 
     private void Update() {
+        glow.Rotate(Vector3.forward * Time.deltaTime * speedRotation);
     
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
+        int index = 0;
+        switch (course) {
+            case LevelSettings.LevelName.RustEmpire : index = 0; break;
+            case LevelSettings.LevelName.NeonRepublic : index = 1; break;
+            case LevelSettings.LevelName.WoodenKingdom : index = 2; break;
+        }
         Application.LoadLevel(index);
     }
 }
