65fc19345c8ea7aa298547dac45774cd3017faae
almyu <hire@indeed.li>
2014-08-24 19:52:28 +0700

    Add pinning input

---
+++ Assets/Scripts/ActionInput.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ActionInput : MonoBehaviour {
+
+    public UnityEvent onPin;
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0))
+            onPin.Invoke();
+    }
+}
