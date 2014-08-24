8b813ef67a4378eda4bac11ec95747c18fd511ed
almyu <hire@indeed.li>
2014-08-24 18:38:28 +0700

    Tweak physics

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -7,6 +7,7 @@ public class Car : MonoBehaviour {
     public Transform inner;
     public bool directControl = true;
     public float acceleration = 20.0f;
+    public float turningSpeed = 25.0f;
 
     [System.Serializable]
     public class OnDriveEvent : UnityEvent<bool> {}
@@ -46,10 +47,10 @@ public class Car : MonoBehaviour {
 
             var vel = cachedBody.velocity;
             if (vel.sqrMagnitude >= Mathf.Epsilon)
-                inner.up = Vector2.Lerp(inner.up, vel.normalized, Time.fixedDeltaTime * acceleration / 1.5f);
+                inner.up = Vector2.Lerp(inner.up, vel.normalized, Time.fixedDeltaTime * turningSpeed * 1.5f);
         }
         else {
-            inner.Rotate(Vector3.back, control.x * Time.fixedDeltaTime * Vector3.Dot(cachedBody.velocity, inner.up) * acceleration / 2.0f);
+            inner.Rotate(Vector3.back, control.x * Time.fixedDeltaTime * Vector3.Dot(cachedBody.velocity, inner.up) * turningSpeed);
             cachedBody.AddForce(inner.up * Time.fixedDeltaTime * control.y * acceleration, ForceMode2D.Impulse);
         }
     }
