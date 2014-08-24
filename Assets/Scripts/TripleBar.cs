8c85b8c59d2862716266c8cf5a9f4b033c97ef25
Anton Tarasov <uchiro.me@gmail.com>
2014-08-25 05:46:34 +0700

    Add triple bar and world stats

---
+++ Assets/Scripts/TripleBar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TripleBar : MonoBehaviour {
+
+    public RectTransform rust;
+    public RectTransform neon;
+    public RectTransform wooden;
+
+    private void Update() {
+        var stats = WorldsStats.instance.statsNormalized;
+        rust.anchorMin = new Vector3(0.0f, 0,0f);
+        rust.anchorMax = new Vector3(stats.x, 1,0f);
+        neon.anchorMin = new Vector3(stats.x, 0,0f);
+        neon.anchorMax = new Vector3(stats.x + stats.y, 1,0f);
+        wooden.anchorMin = new Vector3(stats.x + stats.y, 0,0f);
+        wooden.anchorMax = new Vector3(1.0f, 1,0f);
+    }
+}
---
+++ Assets/Scripts/WorldsStats.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldsStats : MonoBehaviour {
+
+    public static WorldsStats instance;
+
+    public Vector3 stats;
+
+    public float increasingRate = 4.0f;
+
+    private int homeLevel;
+
+    private void Awake() {
+        instance = this;
+
+        homeLevel = PlayerPrefs.GetInt("HomeLevel", 0);
+
+        stats = new Vector3(100.0f, 100.0f, 100.0f);
+    }
+
+    private void Update() {
+        stats = new Vector3(homeLevel != 0 ? stats.x + Time.deltaTime * increasingRate : stats.x,
+                            homeLevel != 1 ? stats.y + Time.deltaTime * increasingRate : stats.y,
+                            homeLevel != 2 ? stats.z + Time.deltaTime * increasingRate : stats.z);
+    }
+
+    public Vector3 statsNormalized {
+        get {
+            var s = stats.x + stats.y + stats.z;
+        
+            return new Vector3(stats.x / s, stats.y / s, stats.z / s);
+        }
+    }
+}
