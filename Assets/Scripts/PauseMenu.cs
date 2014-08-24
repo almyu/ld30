4ef9e1d37a8cf8830c9e9ab178a461a93fa024c5
Anton Tarasov <uchiro.me@gmail.com>
2014-08-25 06:37:38 +0700

    Add pause menu with support of gamepad

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject window;
+
+    private float timeScale;
+
+    private void Awake() {
+        timeScale = Time.timeScale;
+    }
+
+    private void Update() {
+        if (Input.GetButtonDown("Pause")) {
+            if (Time.timeScale == 0.0f) {
+                Time.timeScale = timeScale;
+                window.SetActive(false);
+            }
+            else {
+                Time.timeScale = 0.0f;
+                window.SetActive(true);
+            }
+        }
+
+        if (Input.GetButtonDown("Accept") && Time.timeScale == 0.0f)
+            LoadMainMenu();
+    }
+
+    public void LoadMainMenu() {
+        Time.timeScale = timeScale;
+        Application.LoadLevel(3);
+    }
+
+    public void ReturnToGame() {
+        Time.timeScale = timeScale;
+        window.SetActive(false);
+    }
+}
