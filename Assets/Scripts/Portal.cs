1732a19d5e58b004932650e3bcbf75cb6f8ef091
Anton Tarasov <uchiro.me@gmail.com>
2014-08-24 22:44:54 +0700

    Add levels prefab and portals

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+
+    public int index = 1;
+
+    private void Awake() {
+    
+    }
+
+    private void Update() {
+    
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Application.LoadLevel(index);
+    }
+}
