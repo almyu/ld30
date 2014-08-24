26200691b76d735b41168f2837777769301f952c
Anton Tarasov <uchiro.me@gmail.com>
2014-08-24 23:13:12 +0700

    Add spawn portals

---
+++ Assets/Scripts/LevelSettings.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSettings : MonoBehaviour {
+
+    public static LevelSettings instance;
+
+    public enum LevelName{
+        RustEmpire,
+        NeonRepublic,
+        WoodenKingdom
+    };
+
+    public LevelName levelName;
+
+    private void Awake() {
+        instance = this;
+    }
+}
