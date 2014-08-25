96b750ca6648ddb73ea44349a50d27fdd588911a
almyu <hire@indeed.li>
2014-08-26 05:44:56 +0700

    Add wrecking ball script

---
+++ Assets/Scripts/WreckingBall.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class WreckingBall : MonoBehaviour {
+
+    public LayerMask layers = -1;
+    public float damage = 20.0f;
+
+    private void OnCollisionEnter2D(Collision2D coll) {
+        if (((1 << coll.gameObject.layer) & layers.value) == 0) return;
+
+        var car = coll.gameObject.GetComponent<Car>();
+        if (car == null) return;
+
+        car.Hit(coll.relativeVelocity.magnitude * damage);
+    }
+}
