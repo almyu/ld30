93f622df0b987728d189de6c3777b167dc136f9c
almyu <hire@indeed.li>
2014-08-26 02:00:22 +0700

    Add ultimate action logic

---
+++ Assets/Scripts/ActionUltimate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem), typeof(Collider2D))]
+public class ActionUltimate : MonoBehaviour {
+
+    public LayerMask enemyLayers = -1;
+    public float damage = 10.0f;
+
+    private void OnTriggerStay2D(Collider2D coll) {
+        if (((1 << coll.gameObject.layer) & enemyLayers.value) == 0) return;
+
+        var car = coll.GetComponent<Car>();
+        if (car == null) return;
+
+        car.Hit(damage);
+    }
+}
