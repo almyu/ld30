a00a96633aa62b0bfaca91165c7a65c68b9b1182
Anton Tarasov <uchiro.me@gmail.com>
2014-08-26 00:36:22 +0700

    Add hp

---
+++ Assets/Scripts/PlayerHP.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHP : MonoBehaviour {
+
+    public SpriteRenderer pie;
+
+    private Car cachedCar;
+    private float max;
+
+    private void Awake() {
+        cachedCar = GetComponent<Car>();
+        max = cachedCar.health;
+    }
+
+    private void Update() {
+        pie.color = pie.color.ReplaceR(cachedCar.health / max);
+    }
+}
