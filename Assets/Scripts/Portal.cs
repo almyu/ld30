d9e7b7fa2b226a9a178a0710c65d7d5eda38a148
Anton Tarasov <uchiro.me@gmail.com>
2014-08-25 23:17:59 +0700

    Change portal

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -6,11 +6,17 @@ public class Portal : MonoBehaviour {
     public int course;
 
     public Transform glow;
+    public Transform centerLayer;
+    public Transform upLayer;
 
     public float speedRotation = 1.0f;
+    public float speedCenterRotation = 1.0f;
+    public float speedUpRotation = 1.0f;
 
     private void Update() {
         glow.Rotate(Vector3.forward * Time.deltaTime * speedRotation);
+        centerLayer.Rotate(Vector3.forward * Time.deltaTime * speedCenterRotation);
+        upLayer.Rotate(Vector3.forward * Time.deltaTime * speedUpRotation);
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
