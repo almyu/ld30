10a0d9c4de2c1593fbedb0ffd49f4d4b859a9b92
almyu <hire@indeed.li>
2014-08-26 02:22:37 +0700

    Update car effects lifetime

--- Assets/Scripts/CarEffects.cs
+++ Assets/Scripts/CarEffects.cs
@@ -12,13 +12,28 @@ public class CarEffects : MonoBehaviour {
     }
 
     public void SpawnPrefab(GameObject prefab) {
-        Instantiate(prefab, cachedXf.position, cachedXf.rotation);
+        var obj = (GameObject) Instantiate(prefab, cachedXf.position, cachedXf.rotation);
+
+        var pss = obj.GetComponentsInChildren<ParticleSystem>();
+        if (pss.Length != 0) {
+            var lifetime = 0.0f;
+
+            foreach (var ps in pss)
+                if (!ps.loop)
+                    if (lifetime < ps.duration)
+                        lifetime = ps.duration;
+
+            if (lifetime > 0.0f)
+                Destroy(obj, lifetime);
+        }
     }
 
     public void SpawnSparks(float force, Collision2D collision) {
         var rotation = Quaternion.LookRotation(collision.relativeVelocity.normalized, Vector3.back);
         var obj = (GameObject) Instantiate(sparksPrefab, collision.contacts[0].point, rotation);
-        obj.GetComponent<ParticleSystem>().maxParticles = Mathf.CeilToInt(force * 10.0f);
-        Destroy(obj, 1.0f);
+        var ps = obj.GetComponent<ParticleSystem>();
+
+        ps.maxParticles = Mathf.CeilToInt(force * 10.0f);
+        Destroy(obj, ps.duration);
     }
 }
