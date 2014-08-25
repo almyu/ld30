b4dc3788cc4ee0d41cf946a368d631e8f102232a
Anton Tarasov <uchiro.me@gmail.com>
2014-08-26 02:47:28 +0700

    Add pie in enemy

--- Assets/Scripts/HealthIndicator.cs
+++ Assets/Scripts/HealthIndicator.cs
@@ -11,11 +11,6 @@ public class HealthIndicator : MonoBehaviour {
     private void Awake() {
         cachedCar = GetComponent<Car>();
         max = cachedCar.health;
-
-        var id = PlayerPrefs.GetInt("HomeLevel", 0);
-
-        var color = PlayerSettings.instance.playerPieColors[id];
-        pie.sharedMaterial.SetVector("_RedSector", new Vector4(color.r, color.g, color.b, color.a));
     }
 
     private void Update() {
--- Assets/Scripts/LevelSettings.cs
+++ Assets/Scripts/LevelSettings.cs
@@ -16,11 +16,19 @@ public class LevelSettings : MonoSingleton<LevelSettings> {
 
     public int levelIndex;
 
+    private Material mat;
+
     private void Awake() {
 
         var id = PlayerPrefs.GetInt("HomeLevel", 0);
 
         var player = GameObject.Find("Player/Inner/Visual").transform;
+
+        var pie = GameObject.Find("Player/Pie").GetComponent<SpriteRenderer>();
+
+        var color = PlayerSettings.instance.playerPieColors[id];
+        mat = pie.material;
+        mat.SetVector("_RedSector", new Vector4(color.r, color.g, color.b, color.a));
 
         var skin = Instantiate(PlayerSettings.instance.skins[id], new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity) as GameObject;
         skin.transform.parent = player;
