c5b0922132c59c4ad63c3ade3e5182925b12b59f
almyu <hire@indeed.li>
2014-08-26 04:36:50 +0700

    Update physics

--- Assets/Scripts/CarPhysics.cs
+++ Assets/Scripts/CarPhysics.cs
@@ -4,6 +4,8 @@ using UnityEngine.Events;
 [RequireComponent(typeof(Rigidbody2D)), RequireComponent(typeof(Collider2D))]
 public class CarPhysics : MonoBehaviour {
 
+    public Transform innerXf;
+
     public LayerMask damagingLayers = -1;
     public float deadZone = 90.0f;
 
@@ -15,18 +17,17 @@ public class CarPhysics : MonoBehaviour {
     public class OnImpactDetailedEvent : UnityEvent<float, Collision2D> {}
     public OnImpactDetailedEvent onImpactDetailed;
 
-    private Transform cachedXf;
-
     private void Awake() {
-        cachedXf = transform;
+        if (innerXf == null)
+            innerXf = transform.Find("Inner").transform;
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
         var mask = 1 << collision.gameObject.layer;
         if ((damagingLayers.value & mask) == 0) return;
-        if (Vector2.Angle(cachedXf.up, collision.relativeVelocity) < deadZone * 0.5f) return;
+        if (Vector2.Angle(innerXf.up, collision.relativeVelocity) < deadZone * 0.5f) return;
 
-        var force = Mathf.Max(0.0f, -Vector2.Dot(cachedXf.up, collision.relativeVelocity));
+        var force = Mathf.Max(0.0f, -Vector2.Dot(innerXf.up, collision.relativeVelocity));
 
         onImpact.Invoke(force);
         onImpactDetailed.Invoke(force, collision);
