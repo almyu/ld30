313ca19fa6741e1ba69697304079f7a5f87835f6
almyu <hire@indeed.li>
2014-08-25 22:35:07 +0700

    Tweak impact shake

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,6 +5,8 @@ public class CameraFollow : MonoSingleton<CameraFollow> {
     public Rigidbody2D target;
     public float sensitivity = 0.1f;
     public float smoothness = 15.0f;
+    public float shakeExponent = 3.0f;
+    public float shakePower = 1.0f;
 
     private Transform cachedXf;
     private Vector3 fromTarget;
@@ -19,10 +21,10 @@ public class CameraFollow : MonoSingleton<CameraFollow> {
 
     private void FixedUpdate() {
         detachTimer = Mathf.Max(0.0f, detachTimer - Time.fixedDeltaTime);
-        var catchup = Mathf.Clamp01(detachTimer * detachTimer);
+        var catchup = Mathf.Clamp01(Mathf.Pow(detachTimer, shakeExponent));
 
         var desiredPosition = target.transform.position + fromTarget + (Vector3) target.velocity * sensitivity * (1.0f - catchup);
-        cachedXf.position = Vector3.Lerp(cachedXf.position, desiredPosition, Time.fixedDeltaTime * smoothness) + Random.insideUnitSphere * catchup;
+        cachedXf.position = Vector3.Lerp(cachedXf.position, desiredPosition, Time.fixedDeltaTime * smoothness) + Random.insideUnitSphere * shakePower * catchup;
     }
 
     public void DetachTemporarily(float duration) {
