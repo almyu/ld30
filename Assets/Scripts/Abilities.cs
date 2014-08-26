acd67cd4bb5d7f61c35a122dcb576ae844f9f84f
Merzlot <nicolayevaa@mail.ru>
2014-08-27 02:28:35 +0800

    Fix cooldown

--- Assets/Scripts/Abilities.cs
+++ Assets/Scripts/Abilities.cs
@@ -3,27 +3,36 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class Abilities : MonoBehaviour {
-	public Color inactiveColor, activeColor;
+    public Color inactiveColor, activeColor;
 
-	public Image[] imageActions = new Image[4];
-	public Text[] textActions = new Text[4];
+    public Image[] imageActions = new Image[4];
+    public Text[] textActions = new Text[4];
 
-	public Action[] actions = new Action[4];
+    public Action[] actions = new Action[4];
 
     private void Awake() {
-        actions = FindObjectsOfType<Action>();
+        var names = new[] { "Charge", "Chainball", "Barrier", "Ultimate" };
+
+        foreach (var action in FindObjectsOfType<Action>()) {
+            for (int i = 0; i < 4; ++i) {
+                if (action.name == names[i]) {
+                    actions[i] = action;
+                    break;
+                }
+            }
+        }
     }
 
     private void Update() {
-    	for (int i = 0; i < actions.Length; ++i) {
-    		if(actions[i].cooldownTimer != 0.0f) {
-				imageActions[i].color = inactiveColor;
-				textActions[i].text = actions[i].cooldown.ToString();
-    		}
-    		else {
-    			imageActions[i].color = activeColor;
-				textActions[i].text = "";
-    		}
-    	}
+        for (int i = 0; i < actions.Length; ++i) {
+            if(actions[i].cooldownTimer != 0.0f) {
+                imageActions[i].color = inactiveColor;
+                textActions[i].text = actions[i].cooldownTimer.ToString("n1");
+            }
+            else {
+                imageActions[i].color = activeColor;
+                textActions[i].text = "";
+            }
+        }
     }
 }
--- Assets/Scripts/Action.cs
+++ Assets/Scripts/Action.cs
@@ -45,5 +45,6 @@ public class Action : MonoBehaviour {
 
         onUse.Invoke();
         durationTimer = duration;
+        cooldownTimer = cooldown;
     }
 }
