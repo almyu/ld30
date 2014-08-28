d8fb6493f37d8c0b1a35123ca24beda395499601
almyu <hire@indeed.li>
2014-08-29 04:15:08 +0800

    Tweak exhaust particle size on jump

---
+++ Assets/Scripts/ParticleTweaker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ParticleTweaker : MonoBehaviour {
+
+    public float sizeFactor = 1.0f;
+
+    private ParticleSystem[] cachedSystems;
+    private float[] cachedSizes;
+
+    private void Start() {
+        cachedSystems = GetComponentsInChildren<ParticleSystem>();
+        cachedSizes = System.Array.ConvertAll(cachedSystems, ps => ps.startSize);
+    }
+
+    private void Update() {
+        for (int i = 0; i < cachedSystems.Length; ++i)
+            cachedSystems[i].startSize = cachedSizes[i] * sizeFactor;
+    }
+}
