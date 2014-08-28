e134899ca5454fc2720b599a10c93a39a8751781
almyu <hire@indeed.li>
2014-08-29 04:47:42 +0800

    Fix stomp sorting

---
+++ Assets/Scripts/ParticleSorter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleSorter : MonoBehaviour {
+
+    public string sortingLayerName;
+
+    private void Awake() {
+        particleSystem.renderer.sortingLayerName = sortingLayerName;
+    }
+}
