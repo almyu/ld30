711ca2100a9350cf8cceb5ce2e629d7b5f1c1308
almyu <hire@indeed.li>
2014-08-30 03:27:02 +0800

    Fix jump pad vs charge trigger collision

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class JumpPad : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D other) {
+        if (other.isTrigger) return;
+
         Sounds.instance.PlayJump();
         other.gameObject.GetComponent<Animator>().SetTrigger("Jump");
         Destroy(gameObject);
